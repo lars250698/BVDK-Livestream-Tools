854229258831e7d5a103c401655c343966ab458b
Lars Eppinger <lars@eppinger.dev>
2024-06-16 14:22:09 +0200

    feat: route-dependent page titles

--- src/renderer/src/router/index.js
+++ src/renderer/src/router/index.js
@@ -4,17 +4,29 @@ import LivestreamOptionsView from '../views/LivestreamOptionsView.vue'
 import LowerThirds from '../views/LowerThirds.vue'
 import Scoreboard from '../views/Scoreboard.vue'
 
+const defaultTitle = 'BVDK Livestream Tools'
+
 const routes = [
   { name: 'entry', path: '/', component: LoginView },
   { name: 'options', path: '/options', component: LivestreamOptionsView },
   { name: 'login', path: '/login', component: LoginView },
-  { name: 'lower-thirds', path: '/stream/lower-thirds/:port', component: LowerThirds },
-  { name: 'scoreboard-overall', path: '/scoreboard/overall/:port', component: Scoreboard }
+  { name: 'lower-thirds', path: '/stream/lower-thirds/:port', component: LowerThirds, meta: { title: 'Athletenanzeige' } },
+  { name: 'scoreboard-overall', path: '/scoreboard/overall/:port', component: Scoreboard, meta: { title: 'Scoreboard overall' } }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  console.log(to.meta.title)
+  if (to.meta.title) {
+    document.title = to.meta.title
+  } else {
+    document.title = defaultTitle
+  }
+  next()
 })
 
 export default router
