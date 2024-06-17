bb62e3d8e3b53149845cbaf11aceefacf57f95c5
Lars Eppinger <lars@eppinger.dev>
2024-06-17 03:05:48 +0200

    chore: migrate main and renderer entry points to typescript

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -4,10 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Toast from 'vue-toastification'
+import Toast, { PluginOptions } from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 
-const toastOptions = {
+const toastOptions: PluginOptions = {
   transition: 'Vue-Toastification__bounce',
   maxToasts: 20,
   newestOnTop: true
